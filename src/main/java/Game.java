da8ffc4525a22bd9bdb24d0a2f23b646392760e0
Eden Kang <kg0116@lycle.co.kr>
2021-05-04 23:21:26 +0900

    feature($Game) : Add play
    게임의 총 횟수와 현재시행횟수가 같아질 때까지, 게임을 시행하는 로직 추가

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,4 +8,15 @@ public class Game {
         this.totalCount = totalCount;
     }
 
+    public void play(Cars cars){
+        System.out.println("==== 게임 실행 결과 ====");
+        while(this.totalCount.number != this.currentCount){
+            System.out.println("");
+            System.out.println(String.format("%d 회차 시행!", this.currentCount + 1));
+            cars.go();
+            this.currentCount++;
+        }
+        end(cars);
+    }
+
 }
