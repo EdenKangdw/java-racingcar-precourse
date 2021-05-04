0130df63f6b6a4d242605c529ad3e6748f5c444a
Eden Kang <kg0116@lycle.co.kr>
2021-05-04 22:14:41 +0900

    feature : Add validation check for InputCarName
    입력받은 String이 5자 이하인지 확인하는 로직과 테스트 추가함

--- src/test/java/InputCarNameTest.java
+++ src/test/java/InputCarNameTest.java
@@ -2,18 +2,26 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InputCarNameTest {
+    String names = "오징어,자전거,비행기,오리배";
+    String [] results = {"오징어", "자전거", "비행기", "오리배"};
+    InputCarName carNames = new InputCarName(names);
 
     @Test
     void 자동차_이름_배열_생성_테스트() {
-        String names = "오징어,자전거,비행기,오리배";
-        String [] results = {"오징어", "자전거", "비행기", "오리배"};
-        InputCarName carNames = new InputCarName(names);
-
         int i = 0;
         for(String name : carNames.names){
             Assertions.assertThat(name).isEqualTo(results[i]);
             i++;
         }
+    }
 
+    @Test()
+    void 자동차_이름_5자이하_검증_테스트() {
+        String name = "일이삼사오육칠";
+
+        Assertions.assertThatThrownBy(()
+        -> { carNames.checkLengthUnderStandard(5, name); })
+            .isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("5자 이하의 문자열만 가능합니다");
     }
 }
